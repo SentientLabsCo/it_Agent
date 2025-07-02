af434b553fb0c5e293f1f496138a505e5b53fbdf
Arpit Raghuvanshi <81549335+ArpitTechTrek@users.noreply.github.com>
2025-07-02 23:25:59 +0530

    browse_view.dart page complete.

---
+++ FrontEnd/lib/widgets/link_card.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class LinkCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Color color;
+
+  const LinkCard({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        height: 120,
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Color(0xFF2D2D2D),
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.grey[800]!),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                color: color.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Icon(icon, color: color, size: 20),
+            ),
+            SizedBox(height: 8),
+            Text(
+              title,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 12,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
